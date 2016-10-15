4a302465a76e76484db07d76555189308b82f19c
mutaihillary <mutaihillary@yahoo.com>
2016-10-15 11:54:27 +0300

    app

---
+++ app/__init__.py
@@ -0,0 +1,78 @@
+import os
+import sqlite3
+from flask import Flask, render_template,g,session,url_for,abort,flash,redirect,request
+from flask_sqlalchemy import SQLAlchemy
+
+app = Flask(__name__)
+
+app.config.from_object('config')
+
+db = SQLAlchemy(app)
+
+def connect_db():
+    rv = sqlite3.connect(app.config['DATABASE'])
+    rv.row_factory = sqlite3.Row
+    return rv
+
+def get_db():
+    """Opens a new database connection if there is none yet for the
+    current application context.
+    """
+    if not hasattr(g, 'sqlite_db'):
+        g.sqlite_db = connect_db()
+    return g.sqlite_db
+
+@app.teardown_appcontext
+def close_db(error):
+    """Closes the database again at the end of the request."""
+    if hasattr(g, 'sqlite_db'):
+        g.sqlite_db.close()
+
+
+@app.route('/')
+def index():
+    return render_template('index.html')
+
+
+@app.route('/dashboard',methods=['POST', 'GET'])
+def dashboard():
+    if request.method == 'POST':
+        if not session.get('logged_in'):
+            abort(401)
+        db = get_db()
+        db.execute('insert into entries (title, text) values (?, ?)',
+                   [request.form['title'], request.form['text']])
+        db.commit()
+        flash('New entry was successfully posted')
+        return redirect(url_for('dashboard'))
+
+    elif request.method == 'GET':
+        db = get_db()
+        cur = db.execute('select title, text from entries order by id desc')
+        entries = cur.fetchall()
+        return render_template('dashboard.html', entries=entries)
+    return render_template('dashboard.html')
+"""""
+@app.route('/User/<name>')
+#@login_required
+def user(name):
+    user=User = user.query.filter_by(name=name).first() #user = User.query.filter_by(email=form.email.data).first()
+    if user == None:
+        flash('User %s not found.' % name)
+        return redirect(url_for('index'))
+    posts = [
+        {'author': User, 'body': 'Test post #1'},
+        {'author': User, 'body': 'Test post #2'}
+    ]
+    return render_template('user.html',
+                           user=user,
+                           posts=posts)
+"""""
+@app.errorhandler(404)
+def not_found(error):
+    return render_template('404.html'), 404
+
+from app.auth.controllers import auth as auth_module
+
+app.register_blueprint(auth_module)
+db.create_all()
